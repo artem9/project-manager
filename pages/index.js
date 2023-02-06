a80e7618644bb460f185b8e934c42ebde1575275
Artem Suzdal <artemsuzdal@gmail.com>
2023-02-06 08:34:37 +0200

    MUI ReactJS: Project Manager - Using DatePickers

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import Add from '@mui/icons-material/Add';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import FormGroup from '@mui/material/FormGroup';
 import FilterList from '@mui/icons-material/FilterList';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -17,6 +19,10 @@ import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
+
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import { makeStyles } from 'tss-react/mui';
 
@@ -76,121 +82,166 @@ export default function ProjectManager() {
   const [iOSChecked, setIOSChecked] = useState(false);
   const [androidChecked, setAndroidChecked] = useState(false);
   const [softwareChecked, setSoftwareChecked] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [date, setDate] = useState(null);
 
   return (
-    <Grid container direction="column">
-      <Grid item style={{ marginTop: '2em', marginLeft: '5em' }}>
-        <Typography variant="h1">Projects</Typography>
-      </Grid>
-      <Grid item>
-        <TextField
-          placeholder="Search project deatils or create a new entry."
-          variant="standard"
-          style={{ width: '35em', marginLeft: '5em' }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <Add color="primary" style={{ fontSize: 30 }} />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Grid>
-      <Grid item style={{ marginLeft: '5em', marginTop: '2em' }}>
-        <FormGroup row>
-          <FormControlLabel
-            control={
-              <Switch
-                color="primary"
-                checked={websiteChecked}
-                onChange={() => setWebsiteChecked(!websiteChecked)}
-              />
-            }
-            label="Websites"
-            labelPlacement="start"
-            style={{ marginRight: '5em' }}
-          />
-          <FormControlLabel
-            control={
-              <Switch
-                color="primary"
-                checked={iOSChecked}
-                onChange={() => setIOSChecked(!iOSChecked)}
-              />
-            }
-            label="iOS Apps"
-            labelPlacement="start"
-            style={{ marginRight: '5em' }}
-          />
-          <FormControlLabel
-            control={
-              <Switch
-                color="primary"
-                checked={androidChecked}
-                onChange={() => setAndroidChecked(!androidChecked)}
-              />
-            }
-            label="Android Apps"
-            labelPlacement="start"
-            style={{ marginRight: '5em' }}
-          />
-          <FormControlLabel
-            control={
-              <Switch
-                color="primary"
-                checked={softwareChecked}
-                onChange={() => setSoftwareChecked(!softwareChecked)}
-              />
-            }
-            label="Custom Software"
-            labelPlacement="start"
-          />
-        </FormGroup>
-      </Grid>
-      <Grid
-        item
-        container
-        justifyContent="flex-end"
-        style={{ marginTop: '5em' }}
-      >
-        <Grid item style={{ marginRight: 75 }}>
-          <FilterList color="secondary" style={{ fontSize: 50 }} />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Grid container direction="column">
+        <Grid item style={{ marginTop: '2em', marginLeft: '5em' }}>
+          <Typography variant="h1">Projects</Typography>
         </Grid>
-      </Grid>
-      <Grid item style={{ marginBottom: '15em' }}>
-        <TableContainer component={Paper} elevation={0}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Name</TableCell>
-                <TableCell align="center">Date</TableCell>
-                <TableCell align="center">Service</TableCell>
-                <TableCell align="center">Features</TableCell>
-                <TableCell align="center">Complexity</TableCell>
-                <TableCell align="center">Platforms</TableCell>
-                <TableCell align="center">Users</TableCell>
-                <TableCell align="center">Total</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell align="center">{row.name}</TableCell>
-                  <TableCell align="center">{row.date}</TableCell>
-                  <TableCell align="center">{row.service}</TableCell>
-                  <TableCell align="center" style={{ maxWidth: '5em' }}>
-                    {row.features}
-                  </TableCell>
-                  <TableCell align="center">{row.complexity}</TableCell>
-                  <TableCell align="center">{row.platforms}</TableCell>
-                  <TableCell align="center">{row.users}</TableCell>
-                  <TableCell align="center">{row.total}</TableCell>
+        <Grid item>
+          <TextField
+            placeholder="Search project deatils or create a new entry."
+            variant="standard"
+            style={{ width: '35em', marginLeft: '5em' }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => setDialogOpen(true)}
+                >
+                  <Add color="primary" style={{ fontSize: 30 }} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item style={{ marginLeft: '5em', marginTop: '2em' }}>
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Switch
+                  color="primary"
+                  checked={websiteChecked}
+                  onChange={() => setWebsiteChecked(!websiteChecked)}
+                />
+              }
+              label="Websites"
+              labelPlacement="start"
+              style={{ marginRight: '5em' }}
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  color="primary"
+                  checked={iOSChecked}
+                  onChange={() => setIOSChecked(!iOSChecked)}
+                />
+              }
+              label="iOS Apps"
+              labelPlacement="start"
+              style={{ marginRight: '5em' }}
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  color="primary"
+                  checked={androidChecked}
+                  onChange={() => setAndroidChecked(!androidChecked)}
+                />
+              }
+              label="Android Apps"
+              labelPlacement="start"
+              style={{ marginRight: '5em' }}
+            />
+            <FormControlLabel
+              control={
+                <Switch
+                  color="primary"
+                  checked={softwareChecked}
+                  onChange={() => setSoftwareChecked(!softwareChecked)}
+                />
+              }
+              label="Custom Software"
+              labelPlacement="start"
+            />
+          </FormGroup>
+        </Grid>
+        <Grid
+          item
+          container
+          justifyContent="flex-end"
+          style={{ marginTop: '5em' }}
+        >
+          <Grid item style={{ marginRight: 75 }}>
+            <FilterList color="secondary" style={{ fontSize: 50 }} />
+          </Grid>
+        </Grid>
+        <Grid item style={{ marginBottom: '15em' }}>
+          <TableContainer component={Paper} elevation={0}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Name</TableCell>
+                  <TableCell align="center">Date</TableCell>
+                  <TableCell align="center">Service</TableCell>
+                  <TableCell align="center">Features</TableCell>
+                  <TableCell align="center">Complexity</TableCell>
+                  <TableCell align="center">Platforms</TableCell>
+                  <TableCell align="center">Users</TableCell>
+                  <TableCell align="center">Total</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell align="center">{row.name}</TableCell>
+                    <TableCell align="center">{row.date}</TableCell>
+                    <TableCell align="center">{row.service}</TableCell>
+                    <TableCell align="center" style={{ maxWidth: '5em' }}>
+                      {row.features}
+                    </TableCell>
+                    <TableCell align="center">{row.complexity}</TableCell>
+                    <TableCell align="center">{row.platforms}</TableCell>
+                    <TableCell align="center">{row.users}</TableCell>
+                    <TableCell align="center">{row.total}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Typography variant="h1" gutterBottom>
+                Add a new project
+              </Typography>
+            </Grid>
+          </Grid>
+          <DialogContent>
+            <Grid container>
+              <Grid item container direction="column">
+                <Grid item>
+                  <TextField
+                    label="Name"
+                    id="name"
+                    value={name}
+                    variant="standard"
+                    onChange={(event) => setName(event.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <Grid item container direction="column">
+                <Grid item>
+                  <DatePicker
+                    value={date}
+                    onChange={(newDate) => setDate(newDate)}
+                    views={['month', 'day', 'year']}
+                    renderInput={(params) => (
+                      <TextField variant="standard" {...params} />
+                    )}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+          </DialogContent>
+        </Dialog>
       </Grid>
-    </Grid>
+    </LocalizationProvider>
   );
 }
